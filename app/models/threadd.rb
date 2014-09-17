7b6eb10fc12cb591b43998c28e24b470c7fa9d14
Tom Webster <samurailink3@gmail.com>
2014-09-16 20:02:47 -0400

    Post and Image numbers shown on board show page
    Makes it easier to find threads with a lot of content/images.

--- app/models/threadd.rb
+++ app/models/threadd.rb
@@ -3,4 +3,15 @@ class Threadd < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   validates :name, length: { maximum: 70 }, presence: true
   accepts_nested_attributes_for :posts, :allow_destroy => true
+
+  def posts_with_image
+    posts = []
+    self.posts.each do |p|
+      if p.image?
+        posts.push(p)
+      end
+    end
+    return posts
+  end
+
 end
