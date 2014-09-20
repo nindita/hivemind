42b52d3de4c2480ffcdaf237939ffe8c1785600f
Tom Webster <samurailink3@gmail.com>
2014-09-20 12:48:50 -0400

    Seed file updated with list of initial boards.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
-3.times do
-  Board.create!([
-    {name: Faker::App.name, shortcode: Faker::Lorem.word.downcase, description: Faker::Company.bs}
-  ])
-end
-50.times do
-  Threadd.create([
-    {name: Faker::Hacker.verb + " " + Faker::Hacker.adjective + " " + Faker::Hacker.noun + " thread!", board_id: rand(1..3)}
-    ])
-end
-1000.times do
-  Post.create([
-    {content: Faker::Hacker.say_something_smart, image: Faker::Avatar.image, threadd_id: rand(1..50)}
-    ])
-end
+Board.create([
+{name: "Anime", shortcode: "a", description: "The board for all things Anime."},
+{name: "Video Games", shortcode: "v", description: "Old, new, news, console, PC, everything video games."},
+{name: "Comics/Cartoons", shortcode: "co", description: "Indies, DC, Marvel, whatever, we've got you covered."},
+{name: "TV and Movies", shortcode: "tv", description: "From the silver screen to the tube."},
+{name: "Weapons", shortcode: "k", description: "Slice and dice; bang bang; thud. Your personal protection board."},
+{name: "Sports", shortcode: "sp", description: "HE! COULD! GO! ALL! THE! WAY!"},
+{name: "Science", shortcode: "sci", description: "Related news, images, articles, and theory."},
+{name: "Business", shortcode: "biz", description: "HAHA BUSINESS"},
+{name: "Music", shortcode: "mu", description: "What's your jam?"},
+{name: "Gifs", shortcode: "gif", description: "Moving pictures?!? What'll they think of next?"},
+{name: "Fitness", shortcode: "fit", description: "Do you even lift?"},
+{name: "Literature", shortcode: "lit", description: "The book nook"},
+{name: "Technology", shortcode: "g", description: "As technical as you wanna be. Discuss ALL things tech here."},
+{name: "Random", shortcode: "b", description: "The site-wide rules still apply here. Try not to do anything too destructive."}
+])
